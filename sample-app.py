d0eba5cee790b994ee5886c54eb79908d579f84c
Natalia <collazosnatalia996@gmail.com>
2026-08-13 21:19:48 -0500

    fix: arreglos para monitoreo

--- sample-app.py
+++ sample-app.py
@@ -5,17 +5,17 @@ import time
 app  = Flask(__name__)
 
 db_config = {
-  		    "host":  "servidor-bd-ejemplo",
- 			"user": "root",
- 			"password": "sena123",
- 			"database": "adso_db",
-    		"connect_timeout": 3,
+              "host":  "servidor-bd-ejemplo",
+             "user": "root",
+             "password": "sena123",
+             "database": "adso_db",
+            "connect_timeout": 3,
             "cursorclass": pymysql.cursors.DictCursor #devuele los datos como diccionario para HTML 
             
 }
 
 def obtener_conexion():
-	max_retries = 10
+    max_retries = 10
     for attempt in range(max_retries):
         try:
             return pymysql.connect(**db_config)
@@ -25,68 +25,67 @@ def obtener_conexion():
                 time.sleep(3)
             else:
                 raise e
-
 def crear_tabla_si_no_existe():
 
-	try:
-		conn = obtener_conexion()
-		with conn.cursor() as cursor:
-			sql = """
-			CREATE TABLE IF NOT EXISTS aprendices (
-			id INT AUTO_INCREMENT PRIMARY KEY, 
-			nombre_completo VARCHAR(100) NOT NULL,
-			numero_documento VARCHAR(20) NOT NULL,
-			ficha VARCHAR(20) NOT NULL,
-			creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-			);
-			"""
-			cursor.execute(sql)
-			conn.commit()
-			conn.close()
-	except Exception as e:
-		print(f"Error inicializando la base de datos: {e}")
+    try:
+        conn = obtener_conexion()
+        with conn.cursor() as cursor:
+            sql = """
+            CREATE TABLE IF NOT EXISTS aprendices (
+            id INT AUTO_INCREMENT PRIMARY KEY, 
+            nombre_completo VARCHAR(100) NOT NULL,
+            numero_documento VARCHAR(20) NOT NULL,
+            ficha VARCHAR(20) NOT NULL,
+            creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+            """
+            cursor.execute(sql)
+            conn.commit()
+            conn.close()
+    except Exception as e:
+        print(f"Error inicializando la base de datos: {e}")
 
 crear_tabla_si_no_existe()
 
 
 @app.route("/")
 def main():
-	bd_status = ""
-	aprendices = []
+    bd_status = ""
+    aprendices = []
 
-	try:
-		conn = obtener_conexion()
-		with conn.cursor() as cursor:
-			cursor.execute("SELECT * FROM aprendices ORDER BY id DESC")
-			aprendices = cursor.fetchall()
-		conn.close()
-		bd_status = "CONEXION EXITOSA"
-	except Exception as e:
-		bd_status = f"Error de conexión: {e}"
+    try:
+        conn = obtener_conexion()
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM aprendices ORDER BY id DESC")
+            aprendices = cursor.fetchall()
+        conn.close()
+        bd_status = "CONEXION EXITOSA"
+    except Exception as e:
+        bd_status = f"Error de conexión: {e}"
 
-	return render_template("index.html", bd_status = bd_status, aprendices = aprendices)
+    return render_template("index.html", bd_status = bd_status, aprendices = aprendices)
 
 @app.route("/registrar", methods=["POST"])
 def registrar():
-	if request.method == "POST":
-		nombre = request.form["nombre_completo"]
-		documento = request.form["numero_documento"]
-		ficha = request.form["ficha"]
+    if request.method == "POST":
+        nombre = request.form["nombre_completo"]
+        documento = request.form["numero_documento"]
+        ficha = request.form["ficha"]
 
-		try: 
-			conn = obtener_conexion()
-			with conn.cursor() as cursor:
-				sql = "INSERT INTO aprendices (nombre_completo, numero_documento, ficha) VALUES (%s, %s, %s)"
-				cursor.execute(sql, (nombre, documento, ficha))
-			conn.commit()
-			conn.close()
-		except Exception as e:
-			print(f"Error al registrar: {e}")
+        try: 
+            conn = obtener_conexion()
+            with conn.cursor() as cursor:
+                sql = "INSERT INTO aprendices (nombre_completo, numero_documento, ficha) VALUES (%s, %s, %s)"
+                cursor.execute(sql, (nombre, documento, ficha))
+            conn.commit()
+            conn.close()
+        except Exception as e:
+            print(f"Error al registrar: {e}")
 
-		return redirect(url_for("main"))
+        return redirect(url_for("main"))
 
 if __name__== "__main__":
-	app.run(host="0.0.0.0", port = 5050, debug = True)
+    app.run(host="0.0.0.0", port = 5050, debug = True)
 
 
 
