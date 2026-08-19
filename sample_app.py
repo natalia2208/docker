7146492fb34cbaebf03fb95ffe8a098c3d699efb
Natalia <collazosnatalia996@gmail.com>
2026-08-19 16:40:16 -0500

    cambios para test 4

--- sample_app.py
+++ sample_app.py
@@ -59,7 +59,7 @@ def main():
             cursor.execute("SELECT * FROM aprendices ORDER BY id DESC")
             aprendices = cursor.fetchall()
         conn.close()
-        bd_status = "CONEXION EXITOSA Y PRUEBA DE CI/CD EXITOSA"
+        bd_status = "CONEXION EXITOSA Y PRUEBA DE CI/CD y TEST EXITOSO"
     except Exception as e:
         bd_status = f"Error de conexión: {e}"
 
